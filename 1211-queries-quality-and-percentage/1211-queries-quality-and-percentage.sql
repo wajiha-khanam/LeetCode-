e6a4fa3867da72f989ddda8e5e5d696f0cad70fc
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-10-26 20:18:39 +0530

    Time: 447 ms (16.61%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,4 @@
+select query_name, round(avg(rating/position), 2) as quality, round(sum(case when rating < 3 then 1 else 0 end) / count(rating) * 100 , 2) as poor_query_percentage
+from queries
+where query_name is not null
+group by query_name
