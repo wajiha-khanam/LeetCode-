8eeb8354064f6d41aff0719c1c84ed2a60ba9a56
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-11-04 20:18:05 +0530

    Time: 3 ms (99.11%), Space: 16.6 MB (93.70%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
+    
+    # Time = O(n)
+    # Space = O(n)
+        
