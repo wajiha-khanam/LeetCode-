dde3e5ee327cdf7f0995a799975850b5437b705a
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-11-15 12:31:36 +0530

    Time: 401 ms (51.47%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,7 @@
+select a.visited_on as visited_on, sum(b.day_sum) as amount, round(avg(b.day_sum),2) as average_amount
+from
+(select visited_on, sum(amount) as day_sum from customer group by visited_on) a,
+(select visited_on, sum(amount) as day_sum from customer group by visited_on) b
+where datediff(a.visited_on, b.visited_on) between 0 and 6
+group by a.visited_on
+having count(b.visited_on) = 7
